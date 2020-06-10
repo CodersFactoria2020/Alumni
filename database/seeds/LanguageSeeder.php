bfb7dcff58a0c984f2b081360b687154abcae139
Kensyt <61829204+Kensyt@users.noreply.github.com>
2020-06-10 13:42:04 +0200

    Kevin/Crud Language con sedd

--- database/seeds/LanguageSeeder.php
+++ database/seeds/LanguageSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class LanguageSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(LanguageSeeder::class);
+        factory(App\Language::class, 20)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('language','LanguageController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
