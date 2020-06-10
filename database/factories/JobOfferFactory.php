c829c2da11fc7ea06e836b4359b16915a6a4107a
JoseOrtegaVillen <joseortegaaula@.gmail.com>
2020-06-10 12:22:43 +0200

    add basic job offer model

---
+++ database/factories/JobOfferFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\JobOffer;
+use Faker\Generator as Faker;
+
+$factory->define(JobOffer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_06_09_151134_create_job_offers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('job_position');
+            $table->string('job_summary');
+            $table->date('start_day');
+            $table->string('location');
+            $table->string('description');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_offers');
+    }
+}
