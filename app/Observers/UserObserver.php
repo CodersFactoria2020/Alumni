dc6d4190e6840a546d8eed18f3a3a16ef8735689
Mounir El imlahi <munir.elimlahi86@gmail.com>
2020-06-16 19:36:21 +0200

    Mounir/Creating a Default Profile while registrating

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Observers;
+
+use Illuminate\Support\Facades\DB;
+use App\User;
+use App\Profile;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        DB::table('profiles')->insert(
+            ['nickname' => $user->name, 'aboutme' => 'Soy nuevo en Alumni',
+             'web'=>'Redes sociales (instagram, linkedin ...)', 'user_id'=>$user->id
+            ]
+        );
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
--- app/Profile.php
+++ app/Profile.php
@@ -3,14 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+
 
 class Profile extends Model
 {
     protected $fillable=['nickname', 'aboutme','web','social','user_id'];
     public function user(){
-        return $this->belongsTo('App\User'); 
+        return $this->belongsTo('App\User');
     }
     public function events(){
-        return $this->hasMany('App\Event'); 
+        return $this->hasMany('App\Event');
     }
 }
