48e1e3793894f760929f364021a5480023c1610c
ellaquimica <ellaquimica@gmail.com>
2020-06-16 14:26:25 -0300

    Johanna / Test resolucion de conflicto, todo OK

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,57 +14,6 @@
                         </div>
                     @endif
 
-
-                    <h1>Welcome back {{ Auth::user()->name }}<h1>
-                    @can('haveaccess', 'user.index')
-                        <a href="{{route('user.index')}}" class="btn btn-secondary" role="button" >Users</a>
-                    @endcan
-                    @can('haveaccess', 'role.index')
-                        <a href="{{route('role.index')}}" class="btn btn-secondary" role="button" >Roles</a>
-                    @endcan
-                    @can('haveaccess', 'profile.index')
-                         <a href="{{route('profile.index')}}" class="btn btn-secondary" role="button" >Profiles</a>
-                    @endcan
-                    <hr>
-                    <h2>User Data </h2>
-
-                    <h4>Name: {{Auth::user()->name }}</h4>
-                    <h4>Email: {{Auth::user()->email }}</h4>
-
-                    @isset(Auth::user()->roles[0]->name)
-                            <h4>Role: {{Auth::user()->roles[0]->name }}</h4>
-                    @endisset
-
-                    <a href="{{route('user.edit', Auth::user()->id)}}" class="btn btn-secondary" role="button" >Edit User</a>
-
-
-                    <hr>
-                    <h2>Profile Data </h2>
-                    @isset(Auth::user()->profile->id)
-                        <h4>Nickname: {{Auth::user()->profile->nickname}}</h4>
-                        <h4>Web: {{Auth::user()->profile->web}}</h4>
-                        <h4>About me: {{Auth::user()->profile->aboutme}}</h4>
-                        <h4>Social: {{Auth::user()->profile->social}}</h4>
-                        <a href="{{route('profile.edit', Auth::user()->profile->id)}}" class="btn btn-secondary" role="button" >Edit Profile</a>
-                    @endisset
-                    @if(!Auth::user()->profile)
-                        <p> You don't have a profile yet.</p>
-                        <a href="{{route('profile.create')}}" class="btn btn-secondary" role="button" >Create Profile</a>
-                    @endif
-                    <hr>
-                    <h2>My Events: </h2>
-
-                    @isset (Auth::user()->profile->id)
-                        @isset($events)
-                            @foreach ($events as $event)
-                                @if ($event->creator_id===Auth::user()->profile->id)
-                                    <h4>Name: {{$event->name}}</h4>
-                                    <a href="{{route('event.edit', $event->id)}}" >Edit Event </a>
-                                    <a href="{{route('event.show', $event->id)}}" >| View Event</a>
-                                    <hr>
-                                @endif
-                            @endforeach
-
                     <h1><strong>Welcome back {{ Auth::user()->name }}</strong><h1>
                         @can('haveaccess', 'user.index')
                             <a href="{{route('user.index')}}" class="btn btn-outline-secondary" role="button" >Users</a>
@@ -93,7 +42,6 @@
                             <h4><strong>About me: </strong>{{Auth::user()->profile->aboutme}}</h4>
                             <h4><strong>Social: </strong>{{Auth::user()->profile->social}}</h4>
                             <a href="{{route('profile.edit', Auth::user()->profile->id)}}" class="btn btn-secondary" role="button" >Edit Profile</a>
-
                         @endisset
                         @if(!Auth::user()->profile)
                             <p> You don't have a profile yet.</p>
