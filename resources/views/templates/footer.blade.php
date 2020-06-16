c4f89549afc2f56d2623ae9161601253d0317c3c
Axel <axelcayuela@hotmail.com>
2020-06-16 18:02:09 +0200

    Axel/JobOfer Components. JoboferList, jobOferListDetails,JobOferListAdmin, jobOferAdminEdit. files are in Folder: public/, css/

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -19,17 +19,17 @@
     <body>
         @yield('content')
 
-        <div class="footer">
+        <div class="footertemplate">
             <ul class="nav justify-content-end">
                 <li class="nav-item">
-                    <a class="nav-link active"><i class="fa fa-phone"></i> +34 39493 0349</a>
+                     <a class="nav-link telf"><i class="fa fa-phone"></i> +34 39493 0349</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-reset" href="#"><i class="fa fa-info-circle"></i> info</a>
+                    <a class="nav-link" href="#"><i class="fa fa-info-circle"></i> info</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link text-reset" href="#"><i class="fa fa-envelope"></i> email</a>
+                    <a class="nav-link " href="#"><i class="fa fa-envelope"></i> email</a>
                 </li>
 
             </ul>
