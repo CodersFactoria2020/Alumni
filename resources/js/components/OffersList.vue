3becfe722d45fe1cd77847b122b2b7e98518a231
Sofia Carballo <sofia.carballo@live.com>
2020-06-16 11:22:41 +0200

    Display tags.
    Co-authored-by: Francisco
    Co-authored-by: Sofia

--- resources/js/components/OffersList.vue
+++ resources/js/components/OffersList.vue
@@ -5,87 +5,71 @@
 
     <button class="btn btn-info mb-2" @click="getJobOffers"> Update </button>
     <button class="btn btn-primary mb-2" @click="showModalCreate()"> Create </button>
-        <div class="input-group md-form form-sm form-2 pl-0">
-            <input class="form-control my-0 py-1 amber-border" type="text" placeholder="Search" aria-label="Search" v-model="search">
-        </div>
-        <div>
-            <label> Tags: </label>
-            <select name="tag"  class="form-control">
-                <option v-bind:key="i" v-for="(tag, i) in tagList" :value=tag.id> {{tag.name}} </option>
-            </select>
-        </div>
-
-        <br>
-        <div>
-            <ul class="list-group">
-                <li class="list-group-item" v-bind:key="i" v-for="(jobOffer, i) in filteredJobOffers">
-                    <u>Position:</u> {{jobOffer.position}} <br>
-                    <u>Company:</u> {{jobOffer.empresa.name}} <br>
-                    <u>Location:</u> {{jobOffer.location}} <br>
-                    <u>Tags:</u> Laravel, PHP <br>
-                    <br>
-                    <button class="btn btn-danger mb-2" @click="destroy(jobOffer)"> Delete </button>
-                    <button class="btn btn-secondary mb-2" @click="edit(jobOffer)"> Edit </button>
-                    <button class="btn btn-primary mb-2" @click="showModalDetails(jobOffer)"> Show more </button>
-                </li>
-            </ul>
-        </div>
-    <div class="modal fade" id="create">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-body">
-            <label> Position: </label>
-            <input type="text" name="position" class="form-control" v-model="jobOfferToBeCreated.position">
-            <label> Company: </label>
-            <select name="empresa_id"  class="form-control" v-model="jobOfferToBeCreated.empresa_id">
-                <option v-bind:key="i" v-for="(empresa, i) in empresaList" :value=empresa.id> {{empresa.name}} </option>
-            </select>
-            <label> Location: </label>
-            <input type="text" name="location" class="form-control" v-model="jobOfferToBeCreated.location">
-            <label> Description: </label>
-            <input type="text" name="description" class="form-control" v-model="jobOfferToBeCreated.description">
-            <input type="submit" @click="create()">
-          </div>
-        </div>
-      </div>
+    <div class="input-group md-form form-sm form-2 pl-0">
+        <input class="form-control my-0 py-1 amber-border" type="text" placeholder="Search" aria-label="Search" v-model="search">
+    </div>
+    <div>
+        <label> Tags: </label>
+        <select name="tag"  class="form-control">
+            <option v-bind:key="i" v-for="(tag, i) in tagList" :value=tag.id> {{tag.name}} </option>
+        </select>
     </div>
 
-    <div class="modal fade" id="details">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-body">
-            <h5>{{jobOffer.position}}</h5><br>
-            <h5>Company:</h5> {{jobOffer.empresa.name}} <br>
-            <h5>Location:</h5> {{jobOffer.location}} <br>
-            <h5>Description:</h5> {{jobOffer.description}} <br>
-          </div>
-        </div>
-      </div>
+    <br>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item" v-bind:key="i" v-for="(jobOffer, i) in filteredJobOffers">
+                <u>Position:</u> {{jobOffer.position}} <br>
+                <u>Company:</u> {{jobOffer.empresa.name}} <br>
+                <u>Location:</u> {{jobOffer.location}} <br>
+                <u>Tags:</u> Laravel, PHP <br>
+                <br>
+                <button class="btn btn-danger mb-2" @click="destroy(jobOffer)"> Delete </button>
+                <button class="btn btn-secondary mb-2" @click="edit(jobOffer)"> Edit </button>
+                <button class="btn btn-primary mb-2" @click="showModalDetails(jobOffer)"> Show more </button>
+            </li>
+        </ul>
     </div>
 
-    <div class="modal fade" id="edit">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-body">
-            <label> Position: </label>
-            <input type="text" name="position" class="form-control" v-model="jobOffer.position">
-            <label> Company ID: </label>
-            <input type="number" name="empresa_id" class="form-control" v-model="jobOffer.empresa.id">
-            <label> Location: </label>
-            <input type="text" name="location" class="form-control" v-model="jobOffer.location">
-            <label> Description: </label>
-            <input type="text" name="description" class="form-control" v-model="jobOffer.description">
-            <input type="submit" @click="update(jobOffer)">
-          </div>
-        </div>
-      </div>
-    </div>
+    <pop-up popUpId="create">
+        <label> Position: </label>
+        <input type="text" name="position" class="form-control" v-model="jobOfferToBeCreated.position">
+        <label> Company: </label>
+        <select name="empresa_id"  class="form-control" v-model="jobOfferToBeCreated.empresa_id">
+            <option v-bind:key="i" v-for="(empresa, i) in empresaList" :value=empresa.id> {{empresa.name}} </option>
+        </select>
+        <label> Location: </label>
+        <input type="text" name="location" class="form-control" v-model="jobOfferToBeCreated.location">
+        <label> Description: </label>
+        <input type="text" name="description" class="form-control" v-model="jobOfferToBeCreated.description">
+        <input type="submit" @click="create()">
+    </pop-up>
+
+    <pop-up popUpId="details">
+        <h5>{{jobOffer.position}}</h5><br>
+        <h5>Company:</h5> {{jobOffer.empresa.name}} <br>
+        <h5>Location:</h5> {{jobOffer.location}} <br>
+        <h5>Description:</h5> {{jobOffer.description}} <br>
+    </pop-up>
+
+    <pop-up popUpId="edit">
+        <label> Position: </label>
+        <input type="text" name="position" class="form-control" v-model="jobOffer.position">
+        <label> Company ID: </label>
+        <input type="number" name="empresa_id" class="form-control" v-model="jobOffer.empresa.id">
+        <label> Location: </label>
+        <input type="text" name="location" class="form-control" v-model="jobOffer.location">
+        <label> Description: </label>
+        <input type="text" name="description" class="form-control" v-model="jobOffer.description">
+        <input type="submit" @click="update(jobOffer)">
+    </pop-up>
 
   </div>
 
 </template>
 
 <script>
+    import PopUp from './PopUp';
     export default {
       name: 'OffersList',
         data(){
@@ -98,7 +82,7 @@
                 jobOfferToBeCreated: {},
                 search: '',
                 empresaList: [],
-                tagList: [],
+                tagList: []
             }
         },
 
@@ -110,9 +94,6 @@
             },
             clearJobOffer() {
                 this.jobOfferToBeCreated = {};
-                //this.jobOfferList = {
-                //    empresa: {}
-                //}
             },
             showModalEdit(jobOffer) {
                 this.jobOffer = jobOffer
@@ -145,7 +126,6 @@
             },
             edit(jobOffer) {
                 axios.get('/api/jobOffers/' + jobOffer.id).then(response =>{
-                    console.log(response)
                     this.showModalEdit(response.data);
                 });
             },
