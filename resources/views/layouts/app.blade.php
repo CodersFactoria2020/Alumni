9cb80c4a5b3768f6d4ad04a4f295c9dc66e63f94
Axel <axelcayuela@hotmail.com>
2020-06-17 21:21:55 +0200

    Axel/empresa frontend refactor, empresa_index.html and crear_empresa.html width empresa_refactor.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
                         <div class="menu">
                             <nav>
                                 <ul class="menuUl">
-                                    
+
                                     <li><a href="#">Inicio</a></li>
                                     <li><a href="{{Route('perfiles.index')}}">Perfiles</a></li>
                                     <li><a href="{{Route('ofertas.index')}}">Ofertas de Trabajo</a></li>
@@ -69,7 +69,7 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif-->
-                            
+
                             @else
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
