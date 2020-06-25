039fdced77a7781aaa264c6f551643e46af7a879
Diego <diegonskt@gmail.com>
2020-06-25 11:47:17 +0200

    change in dropdown, only logout and edit profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,21 +74,16 @@
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                        <a class="dropdown-item" href="{{ route('dashboard')}}">
+                                            Editar Perfil
+                                        </a>i
+
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
                                         </a>
-
-                                        <a class="dropdown-item" href="{{ route('dashboard') }}">
-                                            User Dashboard
-                                        </a>
-
-                                        @if (auth()->user()->roles[0]->name==="Admin")
-                                            <a class="dropdown-item" href="{{ route('admin') }}">
-                                                Admin Dashboard
-                                            </a>
-                                        @endif
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             @csrf
